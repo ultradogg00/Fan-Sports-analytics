7772240ffbb638f46788a65bdc014f109b4bb7cc
“David <dashieze@gmail.com>
2024-11-26 19:54:46 -0700

    home page template

---
+++ app/page.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className=" bg-">
+      <div>
+        <h1 className=" text-2xl">Fanlaltics</h1>
+      </div>
+     <section>
+
+     </section>
+     <footer>
+      <p>Made by Github:ultradogg00</p>
+     </footer>
+    </main>
+  );
+}
